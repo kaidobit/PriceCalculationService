63db26299ff7b5d2e74bcdb0e0fd8f0f28f5d2f0
Patrick Shaikh <patrick.shaikh@hotmail.de>
2020-06-17 16:25:16 +0200

    create calculator for a price

---
+++ src/main/java/com/pshaikh/price_calculation_service/calculation/PriceCalculator.java
@@ -0,0 +1,28 @@
+package com.pshaikh.price_calculation_service.calculation;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import com.pshaikh.price_calculation_service.web.Item;
+import com.pshaikh.price_calculation_service.web.ItemPrice;
+
+@Service
+public class PriceCalculator {
+	public ItemPrice calculate(Item item) {
+		//when no discount set it to 1
+		if(!(item.getPercentageDiscount() > 0)) {
+			item.setPercentageDiscount(1);
+		}
+		//to provide proper values for this formula
+		float price = item.getBasePrice() * item.getUnits() * (item.getPercentageDiscount() / 100);
+		
+		return new ItemPrice(item.getItemId(), item.getItemDescription(), item.getUnits(), roundToTwoDecimalDigits(price));
+	}
+	
+	private float roundToTwoDecimalDigits(float number) {
+		BigDecimal bd = new BigDecimal(Float.toString(number));
+	    bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);       
+	    return bd.floatValue();
+	}
+}
--- src/main/java/com/pshaikh/price_calculation_service/web/ApiController.java
+++ src/main/java/com/pshaikh/price_calculation_service/web/ApiController.java
@@ -2,12 +2,18 @@ package com.pshaikh.price_calculation_service.web;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pshaikh.price_calculation_service.calculation.PriceCalculator;
+
 @RestController
 public class ApiController {
+	@Autowired
+	PriceCalculator pc;
+	
 	@RequestMapping("/calculate")
 	public ItemPrice calculatePrice(@RequestBody List<Item> itemlist) {
 		//TODO implementation
